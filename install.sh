1822af34ecfa69e6913e8969f886623d405e4955
Victor-Gentle <gentlevictor2000@gmail.com>
2024-07-23 20:08:27 +0100

    updated file

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Update and install necessary packages
 sudo apt-get update
-sudo apt-get install -y nginx docker.io net-tools
+sudo apt-get install -y nginx docker.io net-tools jq
 
 # Create log file
 sudo touch /var/log/devopsfetch.log
@@ -15,13 +15,15 @@ Description=DevOpsFetch Service
 After=network.target
 
 [Service]
-ExecStart=/usr/local/bin/devopsfetch.sh --monitor
+ExecStart=/usr/local/bin/devopsfetch
 Restart=always
+User=root
 
 [Install]
 WantedBy=multi-user.target
 EOL
 
 # Enable and start the service
+sudo systemctl daemon-reload
 sudo systemctl enable devopsfetch.service
 sudo systemctl start devopsfetch.service
